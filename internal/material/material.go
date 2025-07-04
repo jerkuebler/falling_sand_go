89f5b2367638dbcbaea486df94d7e55cb21062e6
Jerome Kuebler <jerkuebler@gmail.com>
2025-07-04 07:10:46 -0400

    renamed substances, update logic chunked

--- internal/material/material.go
+++ internal/material/material.go
@@ -1,18 +1,18 @@
-package Substances
+package Material
 
 type Grain int
 
 const (
-	GrainBlank Grain = iota
-	GrainSand
-	GrainWater
+	Blank Grain = iota
+	Sand
+	Water
 )
 
 func (g Grain) GetColor() []byte {
 	switch g {
-	case GrainSand:
+	case Sand:
 		return []byte{0xde, 0xbd, 0x1a, 0xff} // RGBA for sand
-	case GrainWater:
+	case Water:
 		return []byte{0x00, 0x00, 0xff, 0xff} // RGBA for water
 	default:
 		return []byte{0x00, 0x00, 0x00, 0xff} // RGBA for blank
--- internal/world/world.go
+++ internal/world/world.go
@@ -4,7 +4,7 @@ import (
 	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	Substances "github.com/jerkuebler/falling_sand_go/internal/substances"
+	Material "github.com/jerkuebler/falling_sand_go/internal/material"
 )
 
 type Point struct {
@@ -13,15 +13,15 @@ type Point struct {
 }
 
 type World struct {
-	area      []Substances.Grain
+	area      []Material.Grain
 	width     int
 	height    int
-	heldGrain Substances.Grain
+	heldGrain Material.Grain
 }
 
 func NewWorld(width, height int) *World {
-	area := make([]Substances.Grain, width*height)
-	heldGrain := Substances.GrainSand // Default grain type
+	area := make([]Material.Grain, width*height)
+	heldGrain := Material.Sand // Default grain type
 	w := &World{area: area, width: width, height: height, heldGrain: heldGrain}
 	w.init()
 	return w
@@ -39,7 +39,7 @@ func (w *World) init() {
 
 func (w *World) Update() {
 	// Update logic for the world can be added here
-	next := make([]Substances.Grain, w.width*w.height)
+	next := make([]Material.Grain, w.width*w.height)
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButton0) {
 		mouse_pos := Point{0, 0}
 		mouse_pos.x, mouse_pos.y = ebiten.CursorPosition()
@@ -63,86 +63,199 @@ func (w *World) Update() {
 
 func (w *World) HandlePressedKeys() {
 	if ebiten.IsKeyPressed(ebiten.Key1) {
-		w.heldGrain = Substances.GrainSand
+		w.heldGrain = Material.Sand
 	}
 
 	if ebiten.IsKeyPressed(ebiten.Key2) {
-		w.heldGrain = Substances.GrainWater
+		w.heldGrain = Material.Water
 	}
 }
 
-func (w *World) UpdateSand(x, y int, next []Substances.Grain) {
-	randomOffset := 1
-	if rand.Intn(2) == 0 {
-		randomOffset = -1
-	} else {
-		randomOffset = 1
+func (w *World) UpdateSand(x, y int, next []Material.Grain) {
+
+	if w.BottomScreenCheck(y) {
+		w.SetRelativePosition(x, y, Hold, next) // If at the bottom edge, stay in place
+		return
 	}
-	current_pos := y*w.width + x
-	switch {
-	case y+1 >= w.height:
-		next[current_pos] = w.area[current_pos] // If at the bottom edge, stay in place
-	case w.EmptyBelowCheck(x, y):
-		next[(y+1)*w.width+x] = w.area[current_pos] // If the cell below is empty, move down
+
+	if w.CheckRelativePosition(x, y, Below, Material.Blank) {
+		w.SetRelativePosition(x, y, Below, next) // If the cell below is empty, move down
+		return
+	}
+
+	firstDir := randomDirection()
+	secondDir := firstDir * -1
+
 	// If the cell below and to the left/right is empty, move diagonally
-	case w.HorizontalScreenCheck(x, randomOffset) && w.area[(y+1)*w.width+x+randomOffset] == Substances.GrainBlank:
-		next[(y+1)*w.width+x+randomOffset] = w.area[current_pos]
-	case w.HorizontalScreenCheck(x, randomOffset*-1) && w.area[(y+1)*w.width+x+randomOffset*-1] == Substances.GrainBlank:
-		next[(y+1)*w.width+x+randomOffset*-1] = w.area[current_pos]
+	if w.DiagonalMoveCheck(x, y, firstDir, Material.Blank) {
+		w.SetDiagonalPosition(x, y, firstDir, next)
+		return
+	}
+
+	if w.DiagonalMoveCheck(x, y, secondDir, Material.Blank) {
+		w.SetDiagonalPosition(x, y, secondDir, next)
+		return
+	}
+
 	// If no movement is possible, stay in place
+	w.SetRelativePosition(x, y, Hold, next)
+}
+
+func (w *World) UpdateWater(x, y int, next []Material.Grain) {
+	if w.BottomScreenCheck(y) {
+		w.SetRelativePosition(x, y, Hold, next) // If at the bottom edge, stay in place
+		return
+	}
+
+	if w.CheckRelativePosition(x, y, Below, Material.Blank) {
+		w.SetRelativePosition(x, y, Below, next) // If the cell below is empty, move down
+		return
+	}
+
+	firstDir := randomDirection()
+	secondDir := firstDir * -1
+
+	// If the cell below and to the left/right is empty, move diagonally
+	if w.DiagonalMoveCheck(x, y, firstDir, Material.Blank) {
+		w.SetDiagonalPosition(x, y, firstDir, next)
+		return
+	}
+
+	if w.DiagonalMoveCheck(x, y, secondDir, Material.Blank) {
+		w.SetDiagonalPosition(x, y, secondDir, next)
+		return
+	}
+
+	if w.LateralMoveCheck(x, y, firstDir, Material.Blank, next) {
+		w.SetLateralPosition(x, y, firstDir, next)
+		return
+	}
+	if w.LateralMoveCheck(x, y, firstDir, Material.Blank, next) {
+		w.SetLateralPosition(x, y, firstDir, next)
+		return
+	}
+
+	// If no movement is possible, stay in place
+	w.SetRelativePosition(x, y, Hold, next)
+}
+
+type Direction int
+
+const (
+	Above Direction = iota
+	AboveLeft
+	AboveRight
+	Below
+	BelowLeft
+	BelowRight
+	Left
+	Right
+	Hold
+)
+
+func (d Direction) Delta() (dx, dy int) {
+	switch d {
+	case Above:
+		return 0, -1
+	case AboveLeft:
+		return -1, -1
+	case AboveRight:
+		return 1, -1
+	case Below:
+		return 0, 1
+	case BelowLeft:
+		return -1, 1
+	case BelowRight:
+		return 1, 1
+	case Left:
+		return -1, 0
+	case Right:
+		return 1, 0
 	default:
-		next[current_pos] = w.area[current_pos]
+		return 0, 0
 	}
 }
 
-func (w *World) UpdateWater(x, y int, next []Substances.Grain) {
+func randomDirection() int {
 	randomOffset := 1
 	if rand.Intn(2) == 0 {
 		randomOffset = -1
 	}
-	// top_check := (y - 1) >= 0
-	// bottom_check := y+1 < w.height
-	switch {
-	case y+1 >= w.height:
-		next[y*w.width+x] = Substances.GrainWater // If at the bottom edge, stay in place
-	case w.area[(y+1)*w.width+x] == Substances.GrainBlank:
-		next[(y+1)*w.width+x] = Substances.GrainWater // If the cell below is empty, move down
-	// If the cell below and to the left/right is empty, move diagonally
-	case w.HorizontalScreenCheck(x, randomOffset) && w.area[(y+1)*w.width+x+randomOffset] == Substances.GrainBlank:
-		next[(y+1)*w.width+x+randomOffset] = Substances.GrainWater
-	case w.HorizontalScreenCheck(x, randomOffset*-1) && w.area[(y+1)*w.width+x+randomOffset*-1] == Substances.GrainBlank:
-		next[(y+1)*w.width+x+randomOffset*-1] = Substances.GrainWater
-	case w.HorizontalScreenCheck(x, randomOffset) && w.area[(y)*w.width+x+randomOffset] == Substances.GrainBlank && w.area[(y)*w.width+x-randomOffset] != Substances.GrainBlank && next[(y)*w.width+x+randomOffset] == Substances.GrainBlank:
-		next[(y)*w.width+x+randomOffset] = Substances.GrainWater
-	case w.HorizontalScreenCheck(x, randomOffset*-1) && w.area[(y)*w.width+x+randomOffset*-1] == Substances.GrainBlank && w.area[(y)*w.width+x-randomOffset*-1] == Substances.GrainWater && next[(y)*w.width+x+randomOffset*-1] == Substances.GrainBlank:
-		next[(y)*w.width+x+randomOffset*-1] = Substances.GrainWater
-	default:
-		next[y*w.width+x] = Substances.GrainWater
+	return randomOffset
+}
+
+func (w *World) LateralMoveCheck(x, y, offset int, checkFor Material.Grain, next []Material.Grain) bool {
+	if !w.HorizontalScreenCheck(x, offset) {
+		return false
+	}
+	if offset == 1 {
+		return w.CheckRelativePosition(x, y, Right, checkFor) && w.CheckRelativePositionNextFrame(x, y, Right, checkFor, next)
+	}
+	return w.CheckRelativePosition(x, y, Left, checkFor) && w.CheckRelativePositionNextFrame(x, y, Left, checkFor, next)
+}
+
+func (w *World) SetLateralPosition(x, y, offset int, next []Material.Grain) {
+	if offset == 1 {
+		w.SetRelativePosition(x, y, Right, next)
+	} else {
+		w.SetRelativePosition(x, y, Left, next)
 	}
 }
 
-type RelativePosition struct {
+func (w *World) DiagonalMoveCheck(x, y, offset int, checkFor Material.Grain) bool {
+	if !w.HorizontalScreenCheck(x, offset) {
+		return false
+	}
+	if offset == 1 {
+		return w.CheckRelativePosition(x, y, BelowRight, checkFor)
+	}
+	return w.CheckRelativePosition(x, y, BelowLeft, checkFor)
 }
 
-func (w *World) SetRelativePosition(x, y int)
+func (w *World) SetDiagonalPosition(x, y, offset int, next []Material.Grain) {
+	if offset == 1 {
+		w.SetRelativePosition(x, y, BelowRight, next)
+	} else {
+		w.SetRelativePosition(x, y, BelowLeft, next)
+	}
+}
+
+func (w *World) CheckRelativePositionNextFrame(x, y int, dir Direction, checkFor Material.Grain, next []Material.Grain) bool {
+	dx, dy := dir.Delta()
+	return next[(y+dy)*w.width+x+dx] == checkFor
+}
+
+func (w *World) CheckRelativePosition(x, y int, dir Direction, checkFor Material.Grain) bool {
+	dx, dy := dir.Delta()
+	return w.area[(y+dy)*w.width+x+dx] == checkFor
+}
+
+func (w *World) SetRelativePosition(x, y int, dir Direction, next []Material.Grain) {
+	dx, dy := dir.Delta()
+	next[(y+dy)*w.width+x+dx] = w.area[y*w.width+x]
+}
 
 func (w *World) HorizontalScreenCheck(x, dir int) bool {
 	return x-dir > 0 && x+dir < w.width
 }
 
+func (w *World) BottomScreenCheck(y int) bool {
+	return y+1 >= w.height
+}
+
 func (w *World) EmptyBelowCheck(x, y int) bool {
-	return w.area[(y+1)*w.width+x] == Substances.GrainBlank
+	return w.area[(y+1)*w.width+x] == Material.Blank
 }
 
 func (w *World) Draw(pixels []byte) {
 	for i, v := range w.area {
 		if v != 0 {
-			color := Substances.Grain(v).GetColor()
+			color := Material.Grain(v).GetColor()
 			for j := range 4 {
 				pixels[i*4+j] = color[j]
 			}
 		} else {
-			color := Substances.GrainBlank.GetColor()
+			color := Material.Blank.GetColor()
 			for j := range 4 {
 				pixels[i*4+j] = color[j]
 			}
